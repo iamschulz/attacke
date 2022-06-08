10bfe2eca15f1b49299f2e4089f12098bee9160e
daniel schulz <daniel.schulz@baqend.com>
2022-06-08 21:03:57 +0200

    only cache on install

--- src/ts/sw.ts
+++ src/ts/sw.ts
@@ -1,7 +1,7 @@
 /// <reference lib="webworker" />
 import { getGameAssets } from "./getGameAssets";
 
-const version = "0.0.4";
+const version = "0.0.5";
 const cacheName = `attacke-${version}`;
 const channel = new BroadcastChannel("sw-messages");
 
@@ -9,7 +9,6 @@ const cacheAssets = () => {
 	const assets = [
 		"/",
 		"/index.html",
-		"/credits.html",
 		"/styles.css",
 		"/main.js",
 		"/manifest.json",
@@ -26,15 +25,7 @@ const cacheAssets = () => {
 self.addEventListener("fetch", (event) => {
 	event.respondWith(
 		(async () => {
-			const cacheResponse = await caches.match(event.request);
-			if (cacheResponse) {
-				return cacheResponse;
-			}
-			const response = await fetch(event.request);
-			const cache = await caches.open(cacheName);
-			cache.put(event.request, response.clone());
-			console.log("additional caching", event.request.url);
-			return response;
+			return (await caches.match(event.request)) || fetch(event.request);
 		})()
 	);
 });
